93688c4bc17ad4b5bc7d00244b861576438e4325
= <ios.dev.martins@gmail.com>
2022-11-13 17:51:35 +0100

    Initial Commit

---
+++ NetworkingInApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingInApp
+//
+//  Created by Martin Smith on 13/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
